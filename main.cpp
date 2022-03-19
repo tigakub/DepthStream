a7199322fe803bbf55ba0b335042720cb68fe0ab
Edward Janne <tigakub@mac.com>
2022-03-19 01:11:59 -0700

    Added interpolation of zero values

--- main.cpp
+++ main.cpp
@@ -78,6 +78,7 @@ class StructuredPointCloud
         vertex data[WIDTH * HEIGHT];
 };
 
+/*
 template <int WIDTH=640, int HEIGHT=400, class T>
 T replaceZero(cv::Mat &ioImg, T iReplacement) {
     T max = 0;
@@ -89,6 +90,85 @@ T replaceZero(cv::Mat &ioImg, T iReplacement) {
     }
     return max;
 }
+*/
+
+template <class T>
+class DepthProxy {
+    public:
+        DepthProxy(cv::Mat &iImage) : img(iImage)
+        { }
+    
+        int width() const { return img.cols; }
+        int height() const { return img.rows; }
+    
+        T value(int x, int y) const { return img.ptr<T>()[y * width() + x]; }
+        T &value(int x, int y) { return img.ptr<T>()[y * width() + x]; }
+    
+        T replaceZero(T iReplacement) {
+            T max = 0;
+            T *ptr = img.ptr<T>();
+            int i = width() * height();
+            while(i--) {
+                if(ptr[i] > max) max = ptr[i];
+                if(!ptr[i]) ptr[i] = iReplacement;
+            }
+        }
+    
+        void interpolateZero(int iMaxSearch, T iMaxDif, T iDefaultReplacement) {
+            for(int y = 0; y < height(); y++) {
+                for(int x = 0; x < width(); x++) {
+                    if(!value(x, y)) {
+                        int usex = 0, minx, maxx; T xval1, xval2;
+                        int usey = 0, miny, maxy; T yval1, yval2;
+                        for(int i = 1; (i < iMaxSearch) && (usex < 3) && (usey < 3); i++) {
+                            if((x - i) >= 0) {
+                                if(xval1 = value(x - i, y)) {
+                                    usex |= 1;
+                                    minx = x - i;
+                                }
+                            }
+                            if((x + 1) < width()) {
+                                if(xval2 = value(x + i, y)) {
+                                    usex |= 2;
+                                    maxx = x + i;
+                                }
+                            }
+                            if((y - i) >= 0) {
+                                if(yval1 = value(x, y - i)) {
+                                    usey |= 1;
+                                    miny = y - i;
+                                }
+                            }
+                            if((y + i) < height()) {
+                                if(yval2 = value(x, y + i)) {
+                                    usey |= 2;
+                                    maxy = y + i;
+                                }
+                            }
+                        }
+                        if((usex == 3) && (abs(xval2 - xval1) < iMaxDif)) {
+                            float xstep = float(xval2 - xval1) / float(maxx - minx);
+                            float newx = xval1 + xstep;
+                            for(int ix = minx + 1; ix < maxx; ix++, newx += xstep) {
+                                value(ix, y) = T(newx);
+                            }
+                        } else if((usey == 3) && (abs(yval2 - yval1) < iMaxDif)) {
+                            float ystep = float(yval2 - yval1) / float(maxy - miny);
+                            float newy = yval1 + ystep;
+                            for(int iy = miny + 1; iy < maxy; iy++, newy += ystep) {
+                                value(x, iy) = T(newy);
+                            }
+                        } else {
+                            value(x, y) = iDefaultReplacement;
+                        }
+                    }
+                }
+            }
+        }
+    
+    protected:
+        cv::Mat &img;
+};
 
 int main(int argc, const char * argv[]) {
     cout << "<DepthStream>" << endl;
@@ -271,7 +351,10 @@ int main(int argc, const char * argv[]) {
         // Get the depth frame as a cv2::Mat without any buffer copying
         auto cvDepthImg = depthImgFrame->getFrame(false);
         
-        uint16_t maxZ = replaceZero<640, 400, uint16_t>(cvDepthImg, 33119);
+        DepthProxy<uint16_t> proxy(cvDepthImg);
+        proxy.interpolateZero(20, 100, 33119);
+        
+        // uint16_t maxZ = replaceZero<640, 400, uint16_t>(cvDepthImg, 33119);
         
         // Get a direct pointer to the raw depth buffer
         uint16_t *rawDepth = (uint16_t *) cvDepthImg.ptr<uint16_t>();
